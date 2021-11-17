5cb6f446e6218285c88c178a980c703c3fc2b72e
Jon Earley <me@yelrae.com>
2021-11-17 02:03:21 -0500

    .

--- resolveWebsitePreviewUrl.js
+++ resolveWebsitePreviewUrl.js
@@ -1,6 +1,3 @@
 export default function resolveProductionUrl(document) {
-
-  console.log('resolveProductionUrl', document)
-
   return `https://umich-lib-web-documents.netlify.app/${document.slug.current}`
 }
